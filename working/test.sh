bdce5ff7320672bc6f2e7dfce6627be1279aa131
Eric Son <gruumsh@gmail.com>
2017-02-15 02:48:53 +0000

    working copy?

---
+++ working/test.sh
@@ -0,0 +1,1 @@
+./omxtx -b 3000000 -r 640x360 rtsp://192.168.2.21/axis-media/media.amp rtmp://192.168.2.105/live/transStream.flv
---
+++ working/test1.sh
@@ -0,0 +1,1 @@
+./omxtx rtsp://192.168.2.21/axis-media/media.amp rtmp://192.168.2.105/live/transStream.flv
